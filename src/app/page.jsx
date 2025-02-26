a45b5f2a5d1a25ab1506f814f9a02a8ebf8ae705
BitishaMaharjan <bitisha2005@gmail.com>
2025-02-26 23:08:13 +0545

    chore: seperate the file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,37 +1,10 @@
-"use client";
-import React, { useState } from "react";
+import React from 'react'
+import HomePage from './home/page'
 
-import Header from "@/components/header/Header";
-import UploadZone from "@/components/upload/UploadZone";
-import Tabs from "@/components/tabs/Tab";
-import Footer from "@/components/footer/Footer";
-import BrandTitle from "@/components/hero/BrandTitle";
-
-
-const HomePage = () => {
-  const [activeTab, setActiveTab] = useState("create");
-  const [selectedFile, setSelectedFile] = useState(null);
-
+const page = () => {
   return (
-    <div className="min-h-screen  bg-[#1B2028]">
-      <Header />
+  <HomePage/>
+  )
+}
 
-      <main className="max-w-7xl  mx-auto px-4 pt-8 md:pt-12">
-        <BrandTitle/>
-        <Tabs activeTab={activeTab} onTabChange={setActiveTab} />
-
-        <div className="flex justify-center mb-8 md:mb-12">
-          <button className="bg-[#4CD7E7] text-white px-6 py-3 rounded-full hover:bg-opacity-90 transition-colors">
-            Browse Puzzle Gallery
-          </button>
-        </div>
-
-        {activeTab === "create" && <UploadZone onFileSelect={setSelectedFile} />}
-        {activeTab === "rooms" && <div className="text-center text-white">Rooms feature coming soon...</div>}
-      </main>
-      <Footer/>
-    </div>
-  );
-};
-
-export default HomePage;
+export default page
