2003f27eec166506c3882aec13baef3d5549c6b9
BitishaMaharjan <bitisha2005@gmail.com>
2025-02-26 23:53:42 +0545

    feat: add gallery page

---
+++ src/app/gallery/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Banner from '../../components/banner'
+import Categories from '../../components/categories'
+import PuzzleGallery from '../../components/gallery'
+
+const GalleryPage = () => {
+  return (
+    <>
+    <Banner/>
+    <Categories/>
+    <PuzzleGallery/>
+    </>
+  )
+}
+
+export default GalleryPage
